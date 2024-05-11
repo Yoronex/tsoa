e8ee8b84e24954eb43c2ebe0f0a8635a9a8478ba
Roy Kakkenberg <me@roykakkenberg.nl>
2024-05-11 15:03:47 +0200

    add production build

---
+++ packages/runtime/dist/decorators/extension.d.ts
@@ -0,0 +1,10 @@
+export declare function Extension(_name: string, _value: ExtensionType | ExtensionType[]): Function;
+export type ExtensionType =
+  | string
+  | number
+  | boolean
+  | null
+  | ExtensionType[]
+  | {
+      [name: string]: ExtensionType | ExtensionType[];
+    };
